71ac7cc89fe16350e64789290afd2bd1d622f338
FzTeis <146758681+FzTeis@users.noreply.github.com>
2025-04-22 16:54:17 -0600

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -2,6 +2,7 @@ import { WAMessageStubType } from '@whiskeysockets/baileys'
 import fetch from 'node-fetch'
 
 export async function before(m, { conn, participants, groupMetadata }) {
+  const redes = ""
   if (!m.messageStubType || !m.isGroup) return !0;
   let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://qu.ax/FbQnf.jpg')
   let img = await (await fetch(`${pp}`)).buffer()
