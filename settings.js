73f988067cd5758473e3d4ce4d5a52ab02e3cb96
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-22 10:46:44 -0400

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -45,13 +45,13 @@ global.yukiJadibts = true
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.packname = '⪛✰ 𝐘𝐮𝐤𝐢-𝐒𝐮𝐨𝐮-𝐁𝐨𝐭 ✰⪜'
-global.botname = 'ᥡᥙkі-sᥙ᥆ᥙ-ᑲ᥆𝗍'
-global.wm = 'ৎ୭࠭͢𝐘𝐮𝐤𝐢_𝐒𝐮𝐨𝐮-𝐁𝐨𝐭ⷭ𓆪͟͞ '
-global.author = 'Made By ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜'
-global.dev = '© ⍴᥆ᥕᥱrᥱძ ᑲᥡ ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜'
-global.textbot = 'Yυƙι-Sυσυ-Bσƚ • Powered By ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜'
-global.etiqueta = 'ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜'
+global.packname = '❀sᥙmі sᥲkᥙrᥲsᥲᥕᥲ❀'
+global.botname = '❀ sᥙmі sᥲkᥙrᥲsᥲᥕᥲ ❀ 𝗍'
+global.wm = ''
+global.author = 'Made By ᯓ᮫݃͜ᮨܾ݉🍁ܾ݉𝕷͢𝖊𝖔፝֟፝֟፝֟፝֟፝֟፝֟𝖓𝖊𝖑ܾ݉اﭑلقᬼر'
+global.dev = '© ⍴᥆ᥕᥱrᥱძ ᑲᥡ ᯓ᮫݃͜ᮨܾ݉🍁ܾ݉𝕷͢𝖊𝖔፝֟፝֟፝֟፝֟፝֟፝֟𝖓𝖊𝖑ܾ݉اﭑلقᬼر'
+global.textbot = '⏤͟͟͞͞sᥙmі sᥲkᥙrᥲsᥲᥕᥲ • Powered By ᯓ᮫݃͜ᮨܾ݉🍁ܾ݉𝕷͢𝖊𝖔፝֟፝֟፝֟፝֟፝֟፝֟𝖓𝖊𝖑ܾ݉اﭑلقᬼر'
+global.etiqueta = ''
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
